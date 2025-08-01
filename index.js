070b08c2039a455d536b5be7b8835fa09e44bcf5
samba38 <shivaramchary12@gmail.com>
2025-08-01 09:04:18 +0000

    Solution

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
-const cors=require('cors')
+const cors = require('cors')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 const app = express()
-app.use(cors());
+app.use(cors({
+
+  origin: "*",
+
+  methods: ["GET", "POST", "PUT", "DELETE"],
+
+  credentials: true
+
+}));
 app.use(express.json())
 const dbPath = path.join(__dirname, 'example.db')
 let db = null
